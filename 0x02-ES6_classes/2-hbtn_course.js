e6b6d590746d075c34a6b2ed67f75834025303d6
Shalom <eyoshalom@yahoo.com>
2023-07-28 21:17:03 +0300

    ES6 classes

---
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -0,0 +1,39 @@
+export default class HolbertonCourse {
+  constructor(name, length, students) {
+    if (typeof (name) === 'string') { this._name = name; } else { throw new TypeError(); }
+
+    if (typeof (length) === 'number') { this._length = length; } else { throw new TypeError(); }
+
+    if (Array.isArray(students)) { this._students = students; } else { throw new TypeError(); }
+  }
+
+  get name() { return this._name; }
+
+  get length() { return this._length; }
+
+  get students() { return this._students; }
+
+  set name(name) {
+    if (typeof (name) === 'string') {
+      this._name = name;
+    } else {
+      throw new TypeError('Name must be a string');
+    }
+  }
+
+  set length(length) {
+    if (typeof (length) === 'number') {
+      this._length = length;
+    } else {
+      throw new TypeError('Length must be a number');
+    }
+  }
+
+  set students(students) {
+    if (Array.isArray(students)) {
+      this._students = students;
+    } else {
+      throw new TypeError('students must be an array');
+    }
+  }
+}
